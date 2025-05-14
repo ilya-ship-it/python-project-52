6adf6c4cb4940743d4b2cd6719d1fa853ec21534
ilya-ship-it <tulupov.2712@gmail.com>
2025-05-14 19:36:46 +0300

    step 1

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)t$4ra1jmj(pl@e2%kr90u%n^=s8ssvzhohdw2zj#o(a2r@buf'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'default-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
