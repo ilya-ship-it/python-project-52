b58a56ca16d54bd669d5d81790bfcf3637740845
ilya-ship-it <tulupov.2712@gmail.com>
2025-05-24 19:46:16 +0300

    fix

--- conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-
-project_root = os.path.abspath(os.path.dirname(__file__))
-if project_root not in sys.path:
-    sys.path.insert(0, project_root)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
