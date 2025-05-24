e0c33d58be827ee20951fdb8a329b26aa61b5738
ilya-ship-it <tulupov.2712@gmail.com>
2025-05-24 19:00:04 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
